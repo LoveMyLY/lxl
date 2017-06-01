189c771c2aae2b49c5db19c5fa27b30f7e8909c3
LIN <1228455161@qq.com>
2017-06-01 18:28:38 +0800

    test

--- mygit.sh
+++ mygit.sh
@@ -5,15 +5,28 @@
 # Created Time: 2017年06月01日 星期四 17时43分08秒
 #########################################################################
 #!/bin/bash
-i=0
-echo 0:not init,1:init
-read i
-if [ $i -eq 1 ]
-then
-	git init
-fi
-git add .
-git commit -m $1
-git remote add origin $2
-#git pull origin master
-#git push -u origin $3
+echo tar:压缩文件
+echo git:git
+read menu
+case "$menu" in 
+	"tar" )
+		read name
+		echo 压缩文件名：
+		tar -cf $name .
+		;;
+	"git" )
+		i=0
+		echo 0:初始化
+		echo 1:不初始化
+		read i
+		if [ $i -eq 1 ]
+		then
+			git init
+		fi
+		git add .
+		git commit -m $1
+		git remote add origin $2
+		#git pull origin master
+		#git push -u origin $3
+		;;
+esac
