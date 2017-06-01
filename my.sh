76d51ddf741e6b162928d1c067f3f302fa62b25a
LIN <1228455161@qq.com>
2017-06-01 19:09:29 +0800

    finish tar and git

--- my.sh
+++ my.sh
@@ -5,28 +5,26 @@
 # Created Time: 2017年06月01日 星期四 17时43分08秒
 #########################################################################
 #!/bin/bash
-echo tar:压缩文件
-echo git:git
+echo tar  git
 read menu
 case "$menu" in 
 	"tar" )
+		echo -e "filename:\c"
 		read name
-		echo 压缩文件名：
 		tar -cf $name .
 		;;
 	"git" )
-		i=0
-		echo 0:初始化
-		echo 1:不初始化
-		read i
-		if [ $i -eq 1 ]
-		then
-			git init
-		fi
 		git add .
-		git commit -m $1
-		git remote add origin $2
-		#git pull origin master
-		#git push -u origin $3
+		git commit --allow-empty
+		echo -e "the address of repositories:\c"
+		read address
+		git remote add origin $address
+		echo -e "if upload<y or n>\c"
+		read upload
+		if upload=y
+		then
+			git pull origin master
+		fi
+			#git push -u origin $3
 		;;
 esac
