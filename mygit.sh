737895054a9e714168437acd64156a7ac3998c8d
LIN <1228455161@qq.com>
2017-06-01 17:59:22 +0800

    test

--- mygit.sh
+++ mygit.sh
@@ -5,8 +5,15 @@
 # Created Time: 2017年06月01日 星期四 17时43分08秒
 #########################################################################
 #!/bin/bash
+i=0
 git add .
 git commit -m $1
 git remote add origin $2
-git pull origin master
-git push -u origin $3
+#git pull origin master
+#git push -u origin $3
+i=$4
+if i==0
+then
+	git init
+	echo git init
+fi
